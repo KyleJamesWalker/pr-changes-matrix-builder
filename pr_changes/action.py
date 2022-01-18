dfcf676385d9e4ecfc84cf4f7e824f19fb82e2ca
Kyle James Walker (he/him) <KyleJamesWalker@gmail.com>
2022-01-17 17:53:43 -0800

    Support include and ignore

--- pr_changes/action.py
+++ pr_changes/action.py
@@ -1,4 +1,5 @@
 """PR Changes Primary Action Module."""
+import fnmatch
 import json
 import re
 
@@ -27,6 +28,10 @@ def generate_matrix(changes: List[str]) -> Dict:
     extract_re = re.compile(cfg.input.extract_re)
 
     for c in changes:
+        # Skip files that are ignored or not included
+        if ignore_file(c) or not include_file(c):
+            continue
+
         cur = extract_re.match(c).groupdict()
         if cur[inject_primary_key] not in primary_keys:
             primary_keys.add(cur[inject_primary_key])
@@ -45,3 +50,42 @@ def set_output(name: str, value: object) -> None:
     """Set an action output via stdout."""
     json_text = json.dumps(value)
     print(f"::set-output name={name}::{json_text}")
+
+
+def include_file(fn: str) -> bool:
+    """Check if the filename is an included match.
+
+    Args:
+        fn: Filename to check.
+
+    Returns:
+        True if the file should be included.
+
+    """
+    result = False
+    if get_config().input.paths.include is None:
+        # Default is all files are included
+        result |= True
+
+    for m in get_config().input.paths.include or []:
+        result |= fnmatch.fnmatch(fn, m)
+
+    return result
+
+
+def ignore_file(fn: str) -> bool:
+    """Check if the filename is an excluded match.
+
+    Args:
+        fn: Filename to check.
+
+    Returns:
+        True if the file should be excluded.
+
+    """
+    result = False
+
+    for m in get_config().input.paths.ignore or []:
+        result |= fnmatch.fnmatch(fn, m)
+
+    return result
